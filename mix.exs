ebdbbb0c96582c6d45c747e2883251a17744e0f4
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-11-02 10:42:13 +0100

    Support elixir 0.11.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Postgrex.Mixfile do
   def project do
     [ app: :postgrex,
       version: "0.1.1-dev",
-      elixir: "~> 0.10.3",
+      elixir: "~> 0.10.3 or ~> 0.11.0",
       deps: deps(Mix.env),
       name: "Postgrex",
       source_url: "https://github.com/ericmj/postgrex",
