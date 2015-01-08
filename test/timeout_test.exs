e84ee34643eb1c709330c387c0e114ca44837481
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-01-08 05:54:35 +0100

    Make timeout test work on slow machines

--- test/timeout_test.exs
+++ test/timeout_test.exs
@@ -6,9 +6,12 @@ defmodule TimeoutTest do
     opts = [ database: "postgrex_test" ]
     {:ok, pid} = P.start_link(opts)
 
-    assert {:ok, _} = P.query(pid, "SELECT pg_sleep(0.1)", [], timeout: 200)
-    assert {:timeout, _} = catch_exit P.query(pid, "SELECT pg_sleep(0.1)", [], timeout: 0)
-    :timer.sleep(100)
-    assert {:noproc, _} = catch_exit P.query(pid, "SELECT pg_sleep(0.1)", [], timeout: 200)
+    assert {:ok, _} = P.query(pid, "SELECT 123", [], timeout: 1000)
+    assert {:timeout, _} = catch_exit P.query(pid, "SELECT pg_sleep(1)", [], timeout: 0)
+
+    ref = Process.monitor(pid)
+    assert_receive {:DOWN, ^ref, :process, ^pid, _reason}, 1000
+
+    assert {:noproc, _} = catch_exit P.query(pid, "SELECT 123", [])
   end
 end
