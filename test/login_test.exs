2420b8114f9663e80a612d6a36db8af0eacd9fb4
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-02-12 16:02:27 +0100

    Add test for authentication error message

--- test/login_test.exs
+++ test/login_test.exs
@@ -1,19 +1,43 @@
 defmodule LoginTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   alias Postgrex.Connection, as: P
 
+  defp capture_log(fun) do
+    Logger.remove_backend(:console)
+    fun.()
+    Logger.add_backend(:console, flush: true)
+  end
+
   test "login cleartext password" do
+    Process.flag(:trap_exit, true)
+
     opts = [ hostname: "localhost", username: "postgrex_cleartext_pw",
              password: "postgrex_cleartext_pw", database: "postgres" ]
     assert {:ok, pid} = P.start_link(opts)
     assert :ok = P.stop(pid)
+
+    opts = [ hostname: "localhost", username: "postgrex_cleartext_pw",
+             password: "wrong_password", database: "postgres" ]
+
+    capture_log fn ->
+      assert {:error, %Postgrex.Error{postgres: %{code: "28P01"}}} = P.start_link(opts)
+    end
   end
 
   test "login md5 password" do
+    Process.flag(:trap_exit, true)
+
     opts = [ hostname: "localhost", username: "postgrex_md5_pw",
              password: "postgrex_md5_pw", database: "postgres" ]
     assert {:ok, pid} = P.start_link(opts)
     assert :ok = P.stop(pid)
+
+    opts = [ hostname: "localhost", username: "postgrex_md5_pw",
+             password: "wrong_password", database: "postgres" ]
+
+    capture_log fn ->
+      assert {:error, %Postgrex.Error{postgres: %{code: "28P01"}}} = P.start_link(opts)
+    end
   end
 
   test "parameters" do
