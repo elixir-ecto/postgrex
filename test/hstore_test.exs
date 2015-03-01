92a0cf626269d29ab7a79ccf228c2dfe0d9b3b6f
Chris Maddox <tyre77@gmail.com>
2015-03-01 14:17:10 -0800

    convert hstore to binary

--- test/hstore_test.exs
+++ test/hstore_test.exs
@@ -17,16 +17,27 @@ defmodule HstoreTest do
     assert [{%{}}] = query(~s{SELECT ''::hstore}, [])
     assert [{%{"Bubbles" => 7, "Name" => "Frank"}}] = query(~s{SELECT '"Name" => "Frank", "Bubbles" => "7"'::hstore}, [])
     assert [{%{"non_existant" => nil, "present" => "&accounted_for"}}] = query(~s{SELECT '"non_existant" => NULL, "present" => "&accounted_for"'::hstore}, [])
-    assert [{%{"spaces in the key" => "are easy!"}}] = query(~s{SELECT '"spaces in the key" => "are easy!"'::hstore}, [])
+    assert [{%{"spaces in the key" => "are easy!", "floats too" => 66.6}}] = query(~s{SELECT '"spaces in the key" => "are easy!", "floats too" => "66.6"'::hstore}, [])
+    assert [{%{"this is true" => true, "though not this" => false}}] = query(~s{SELECT '"this is true" => true, "though not this" => false'::hstore}, [])
   end
 
   test "it can decode into a map" do
-    result_map = Decoder.decode ~s("name"=>"Frank","bubbles"=>"seven")
-    assert result_map == %{"name" => "Frank", "bubbles" => "seven"}
+    result_map = Decoder.decode <<0, 0, 0, 2>> <>               # Number of key/value pairs
+                                <<0, 0, 0, 4>> <> "name" <>     # Length of key + key
+                                <<0, 0, 0, 5>> <> "Frank" <>    # Length of value + value
+                                <<0, 0, 0, 7>> <> "bubbles" <>  # Length of key + key
+                                <<0, 0, 0, 5>> <> "seven"       # Length of value + value
+    assert result_map = %{"name" => "Frank", "bubbles" => "seven"}
   end
 
   test "it can decode special values" do
-    result_map = Decoder.decode ~s("limit"=>NULL,"chillin"=>"true","fratty"=>"false")
+    result_map = Decoder.decode <<0, 0, 0, 3>> <>               # Number of key/value pairs
+                                <<0, 0, 0, 5>> <> "limit" <>    # Length of key + key
+                                <<255, 255, 255, 255>> <>       # NULL
+                                <<0, 0, 0, 7>> <> "chillin" <>  # Length of key + key
+                                <<0, 0, 0, 4>> <> "true" <>     # Length of value + value
+                                <<0, 0, 0, 6>> <> "fratty" <>   # Length of key + key
+                                <<0, 0, 0, 5>> <> "false"       # Length of value + value
     assert result_map == %{
       "limit" => nil,
       "chillin"=> true,
@@ -35,18 +46,26 @@ defmodule HstoreTest do
   end
 
   test "it can decode integers" do
-    result_map = Decoder.decode ~s("bubbles"=>"7")
+    result_map = Decoder.decode <<0, 0, 0, 2>> <>               # Number of key/value pairs
+                                <<0, 0, 0, 7>> <> "bubbles" <>  # Length of key + key
+                                <<0, 0, 0, 1>> <> "7"           # Length of value + value
     assert result_map == %{"bubbles" => 7}
   end
 
   test "it can decode floats" do
-    result_map = Decoder.decode ~s("bubbles"=>"7.5")
+    result_map = Decoder.decode <<0, 0, 0, 2>> <>               # Number of key/value pairs
+                                <<0, 0, 0, 7>> <> "bubbles" <>  # Length of key + key
+                                <<0, 0, 0, 3>> <> "7.5"         # Length of value + value
     assert result_map == %{"bubbles" => 7.5}
   end
 
   test "it can encode a map" do
     input_map = Encoder.encode %{"name" => "Frank", "bubbles" => "seven"}
-    assert input_map == ~s("bubbles"=>"seven","name"=>"Frank")
+    assert input_map == <<0, 0, 0, 2>> <>               # Number of key/value pairs
+                        <<0, 0, 0, 7>> <> "bubbles" <>  # Length of key + key
+                        <<0, 0, 0, 5>> <> "seven" <>    # Length of value + value
+                        <<0, 0, 0, 4>> <> "name" <>     # Length of key + key
+                        <<0, 0, 0, 5>> <> "Frank"       # Length of value + value
   end
 
   test "it can encode with nils and booleans" do
@@ -55,7 +74,13 @@ defmodule HstoreTest do
       "chillin"=> true,
       "fratty"=> false
     })
-    assert input_map == ~s("chillin"=>"true","fratty"=>"false","limit"=>NULL)
+    assert input_map == <<0, 0, 0, 3>> <>               # Number of key/value pairs
+                        <<0, 0, 0, 7>> <> "chillin" <>  # Length of key + key
+                        <<0, 0, 0, 4>> <> "true" <>     # Length of value + value
+                        <<0, 0, 0, 6>> <> "fratty" <>   # Length of key + key
+                        <<0, 0, 0, 5>> <> "false" <>    # Length of value + value
+                        <<0, 0, 0, 5>> <> "limit" <>    # Length of key + key
+                        <<255, 255, 255, 255>>          # NULL
   end
 
   test "it can encode integers and floats" do
@@ -63,7 +88,11 @@ defmodule HstoreTest do
       "bubbles" => 7,
       "fragmentation grenades" => 3.5
     }
-    assert input_map == ~s("bubbles"=>"7","fragmentation grenades"=>"3.5")
+    assert input_map == <<0, 0, 0, 2>> <>                               # Number of key/value pairs
+                        <<0, 0, 0, 7>> <> "bubbles" <>                  # Length of key + key
+                        <<0, 0, 0, 1>> <> "7" <>                        # Length of value + value
+                        <<0, 0, 0, 22>> <> "fragmentation grenades" <>  # Length of key + key
+                        <<0, 0, 0, 3>> <> "3.5"                         # Length of value + value
   end
 
 end
