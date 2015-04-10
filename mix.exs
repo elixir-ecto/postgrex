568002c1327251154a572fdb89ee7d360e88e62a
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-04-09 21:27:30 -0500

    Bump to v0.8.2-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Postgrex.Mixfile do
 
   def project do
     [app: :postgrex,
-     version: "0.8.1",
+     version: "0.8.2-dev",
      elixir: "~> 1.0",
      deps: deps,
      build_per_environment: false,
