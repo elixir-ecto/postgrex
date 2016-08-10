0e6b6202d93718a8cc130b7d27bdfe9507bb3b00
James Fish <james@fishcakez.com>
2016-08-10 19:56:21 +0100

    Decode all NaNs to :NaN

--- lib/postgrex/extensions/float4.ex
+++ lib/postgrex/extensions/float4.ex
@@ -4,23 +4,23 @@ defmodule Postgrex.Extensions.Float4 do
   use Postgrex.BinaryExtension, send: "float4send"
 
   def encode(_, :NaN, _, _),
-    do: <<127, 192, 0, 0>>
+    do: <<0::1, 255, 1::1, 0::22>>
   def encode(_, :inf, _, _),
-    do: <<127, 128, 0, 0>>
+    do: <<0::1, 255, 0::23>>
   def encode(_, :"-inf", _, _),
-    do: <<255, 128, 0, 0>>
+    do: <<1::1, 255, 0::23>>
   def encode(_, n, _, _) when is_number(n),
     do: <<n :: float32>>
   def encode(type_info, value, _, _) do
     raise ArgumentError, Postgrex.Utils.encode_msg(type_info, value, "a float")
   end
 
-  def decode(_, <<127, 192, 0, 0>>, _, _),
-    do: :NaN
-  def decode(_, <<127, 128, 0, 0>>, _, _),
+  def decode(_, <<0::1, 255, 0::23>>, _, _),
     do: :inf
-  def decode(_, <<255, 128, 0, 0>>, _, _),
+  def decode(_, <<1::1, 255, 0::23>>, _, _),
     do: :"-inf"
+  def decode(_, <<_::1, 255, _::23>>, _, _),
+    do: :NaN
   def decode(_, <<n :: float32>>, _, _),
     do: n
 end
--- lib/postgrex/extensions/float8.ex
+++ lib/postgrex/extensions/float8.ex
@@ -4,23 +4,23 @@ defmodule Postgrex.Extensions.Float8 do
   use Postgrex.BinaryExtension, send: "float8send"
 
   def encode(_, :NaN, _, _),
-    do: <<127, 248, 0, 0, 0, 0, 0, 0>>
+    do: <<0::1, 2047::11, 1::1, 0::51>>
   def encode(_, :inf, _, _),
-    do: <<127, 240, 0, 0, 0, 0, 0, 0>>
+    do: <<0::1, 2047::11, 0::52>>
   def encode(_, :"-inf", _, _),
-    do: <<255, 240, 0, 0, 0, 0, 0, 0>>
+    do: <<1::1, 2047::11, 0::52>>
   def encode(_, n, _, _) when is_number(n),
     do: <<n :: float64>>
   def encode(type_info, value, _, _) do
     raise ArgumentError, Postgrex.Utils.encode_msg(type_info, value, "a float")
   end
 
-  def decode(_, <<127, 248, 0, 0, 0, 0, 0, 0>>, _, _),
-    do: :NaN
-  def decode(_, <<127, 240, 0, 0, 0, 0, 0, 0>>, _, _),
+  def decode(_, <<0::1, 2047::11, 0::52>>, _, _),
     do: :inf
-  def decode(_, <<255, 240, 0, 0, 0, 0, 0, 0>>, _, _),
+  def decode(_, <<1::1, 2047::11, 0::52>>, _, _),
     do: :"-inf"
+  def decode(_, <<_::1, 2047::11, _::52>>, _, _),
+    do: :NaN
   def decode(_, <<n :: float64>>, _, _),
     do: n
 end
