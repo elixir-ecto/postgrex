003527b0ed139af9393c2bfede65cdbf771addee
Dan Swain <dan.t.swain@gmail.com>
2016-10-02 09:39:23 -0400

    Commenting, update README

--- lib/postgrex/extensions/bit_string.ex
+++ lib/postgrex/extensions/bit_string.ex
@@ -1,13 +1,17 @@
 defmodule Postgrex.Extensions.BitString do
-  use Postgrex.BinaryExtension,
-    [send: "bit_send",send: "varbit_send"]
+  @moduledoc false
+
+  use Postgrex.BinaryExtension, [send: "bit_send",send: "varbit_send"]
   
+  # encode is a straight binary encode plus padding to ensure full bytes
   def encode(_, value, _, _) do
     bit_count = bit_size(value)
     pad = 8 - rem(bit_count, 8)
     << << bit_count :: size(32) >>, << value :: bits >>, << 0 :: size(pad) >> >>
   end
 
+  # decode is a straight binary decode, except if the bit count is
+  # larger than the payload, in which case we pad with zeros
   def decode(_, value, _, _) do
     << bit_count :: size(32), bytes :: binary >> = value
     got_count = bit_size(bytes)
