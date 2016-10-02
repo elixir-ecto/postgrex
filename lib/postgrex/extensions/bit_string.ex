4848405a82304149009260aa6819e97658a98fda
Dan Swain <dan.t.swain@gmail.com>
2016-10-01 21:08:54 -0400

    Further simplification

--- lib/postgrex/extensions/bit_string.ex
+++ lib/postgrex/extensions/bit_string.ex
@@ -5,33 +5,20 @@ defmodule Postgrex.Extensions.BitString do
   
   def encode(_, value, _, _) do
     bit_count = bit_size(value)
-    out = encode_binary(value, <<>>)
-    << << bit_count :: size(32) >>, << out :: bits >> >>
+    pad = 8 - rem(bit_count, 8)
+    << << bit_count :: size(32) >>, << value :: bits >>, << 0 :: size(pad) >> >>
   end
 
   def decode(_, value, _, _) do
     << bit_count :: size(32), bytes :: binary >> = value
-
     got_count = bit_size(bytes)
-    cond do
-      bit_count > got_count ->
-        pad = bit_count - got_count
-        << << bytes :: bits >>, << 0 :: size(pad) >> >>
-      bit_count < got_count ->
-        pad = bit_count - got_count
+
+    if bit_count <= got_count do
         << v :: size(bit_count), _ :: bits >> = bytes
         << v :: size(bit_count) >>
-      true ->
-        bytes
+    else
+      pad = bit_count - got_count
+      << << bytes :: bits >>, << 0 :: size(pad) >> >>
     end
-  end
-
-  defp encode_binary(<< value :: 8, rest :: bits >>, so_far) do
-    encode_binary(rest, << << so_far :: bits >> , << value :: 8 >> >> )
-  end
-  defp encode_binary(<< rest :: bits >>, so_far) do
-    bit_count = bit_size(rest)
-    pad = 8 - bit_count
-    << << so_far :: bits >>, << rest :: bits >>, << 0 :: size(pad) >> >>
   end
 end
