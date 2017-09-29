fd91e5417aea8a8026ab39a691f86bfb1b574d1d
José Valim <jose.valim@plataformatec.com.br>
2017-09-29 22:04:10 +0200

    Use System.cmd/3

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -62,12 +62,12 @@ CREATE SCHEMA test;
 """
 
 cmds = [
-  ~s(psql -U postgres -c "DROP DATABASE IF EXISTS postgrex_test;"),
-  ~s(psql -U postgres -c "DROP DATABASE IF EXISTS postgrex_test_with_schemas;"),
-  ~s(psql -U postgres -c "CREATE DATABASE postgrex_test TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8';"),
-  ~s(psql -U postgres -c "CREATE DATABASE postgrex_test_with_schemas TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8';"),
-  ~s(psql -U postgres -d postgrex_test -c "#{sql}"),
-  ~s(psql -U postgres -d postgrex_test_with_schemas -c "#{sql_with_schemas}")
+  ["-U", "postgres", "-c", "DROP DATABASE IF EXISTS postgrex_test;"],
+  ["-U", "postgres", "-c", "DROP DATABASE IF EXISTS postgrex_test_with_schemas;"],
+  ["-U", "postgres", "-c", "CREATE DATABASE postgrex_test TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8';"],
+  ["-U", "postgres", "-c", "CREATE DATABASE postgrex_test_with_schemas TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8';"],
+  ["-U", "postgres", "-d", "postgrex_test", "-c", sql],
+  ["-U", "postgres", "-d", "postgrex_test_with_schemas", "-c", sql_with_schemas]
 ]
 
 pg_version = System.get_env("PGVERSION")
@@ -76,24 +76,24 @@ pg_path = System.get_env("PGPATH")
 cmds =
   cond do
     !pg_version || String.to_float(pg_version) >= 9.1 ->
-      cmds ++ [~s(psql -U postgres -d postgrex_test_with_schemas -c "CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA test;"),
-               ~s(psql -U postgres -d postgrex_test -c "CREATE EXTENSION IF NOT EXISTS hstore;")]
+      cmds ++ [["-U", "postgres", "-d", "postgrex_test_with_schemas", "-c", "CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA test;"],
+               ["-U", "postgres", "-d", "postgrex_test", "-c", "CREATE EXTENSION IF NOT EXISTS hstore;"]]
     String.to_float(pg_version) == 9.0 ->
-      cmds ++ [~s(psql -U postgres -d postgrex_test -f "#{pg_path}/contrib/hstore.sql")]
+      cmds ++ [["-U", "postgres", "-d", "postgrex_test", "-f", "#{pg_path}/contrib/hstore.sql"]]
     String.to_float(pg_version) < 9.0 ->
-      cmds ++ [~s(psql -U postgres -d postgrex_test -c "CREATE LANGUAGE plpgsql;")]
+      cmds ++ [["-U", "postgres", "-d", "postgrex_test", "-c", "CREATE LANGUAGE plpgsql;"]]
     true ->
       cmds
 end
 
-Enum.each(cmds, fn cmd ->
-  {status, output} = run_cmd.(cmd)
+Enum.each(cmds, fn args ->
+  {output, status} = System.cmd("psql", args, stderr_to_stdout: true)
 
   if status != 0 do
     IO.puts """
     Command:
 
-    #{cmd}
+    psql #{Enum.join(args, " ")}
 
     error'd with:
 
