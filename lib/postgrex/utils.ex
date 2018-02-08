b7d1722a6541f29629f34158b042008d29d50b3d
Dan Janowski <danj@3skel.com>
2018-02-08 18:04:18 -0500

    backport version parser from master

--- lib/postgrex/utils.ex
+++ lib/postgrex/utils.ex
@@ -63,6 +63,8 @@ defmodule Postgrex.Utils do
   def parse_version(version) do
     list =
       version
+      |> String.split(" ")
+      |> hd
       |> String.split(".")
       |> Enum.map(&elem(Integer.parse(&1), 0))
 
