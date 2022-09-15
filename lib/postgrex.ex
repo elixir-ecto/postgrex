61acc591d86c3f952599de2ad21b0ea0f5aaba8a
Greg Rychlewski <greg.rychlewski@gmail.com>
2022-09-15 12:40:28 -0400

    Add `:search_path` option to connection (#620)

--- lib/postgrex.ex
+++ lib/postgrex.ex
@@ -53,6 +53,7 @@ defmodule Postgrex do
           | {:prepare, :named | :unnamed}
           | {:transactions, :strict | :naive}
           | {:types, module}
+          | {:search_path, [String.t()]}
           | {:disconnect_on_error_codes, [atom]}
           | DBConnection.start_option()
 
@@ -157,6 +158,15 @@ defmodule Postgrex do
     * `:types` - The types module to use, see `Postgrex.Types.define/3`, this
       option is only required when using custom encoding or decoding (default:
       `Postgrex.DefaultTypes`);
+
+    * `:search_path` - A list of strings used to set the search path for the connection.
+      This is useful when, for instance, an extension like `citext` is installed in a
+      separate schema. If that schema is not in the connection's search path, Postgrex
+      might not be able to recognize the extension's data type. When this option is `nil`,
+      the search path is not modified. (default: `nil`).
+      See the [PostgreSQL docs](https://www.postgresql.org/docs/current/ddl-schemas.html#DDL-SCHEMAS-PATH)
+      for more details.
+
 
   `Postgrex` uses the `DBConnection` library and supports all `DBConnection`
   options like `:idle`, `:after_connect` etc. See `DBConnection.start_link/2`
