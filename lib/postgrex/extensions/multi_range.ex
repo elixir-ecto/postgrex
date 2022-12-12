e3ba2ae91dd502ec17b9f38c711a19d907f8e71d
Greg Rychlewski <grychle@LLL7WPM64V5M.ngco.com>
2022-12-12 03:51:54 -0500

    add multi_range.ex

---
+++ lib/postgrex/extensions/multi_range.ex
@@ -0,0 +1,70 @@
+defmodule Postgrex.Extensions.MultiRange do
+  import Postgrex.BinaryUtils, warn: false
+
+  @behaviour Postgrex.SuperExtension
+
+  def init(_), do: nil
+
+  def matching(_state), do: [send: "multirange_send"]
+
+  def format(_), do: :super_binary
+
+  def oids(%Postgrex.TypeInfo{base_type: base_oid}, _) do
+    [base_oid]
+  end
+
+  def encode(_) do
+    quote location: :keep do
+      list, [_oid], [type] when is_list(list) ->
+        # encode_value/2 defined by TypeModule
+        encoder = &encode_value(&1, type)
+        unquote(__MODULE__).encode(list, encoder)
+
+      other, _, _ ->
+        raise DBConnection.EncodeError, Postgrex.Utils.encode_msg(other, "a list")
+    end
+  end
+
+  def decode(_) do
+    quote location: :keep do
+      <<len::int32(), data::binary-size(len)>>, [_oid], [type] ->
+        <<_num_ranges::int32(), ranges::binary>> = data
+        # decode_list/2 defined by TypeModule
+        decoder = &decode_list(&1, type)
+        unquote(__MODULE__).decode(ranges, decoder, [])
+    end
+  end
+
+  ## Helpers
+
+  def encode(ranges, encoder) do
+    encoded_ranges =
+      Enum.map(ranges, fn range ->
+        %{lower: lower, upper: upper} = range
+        lower = if is_atom(lower), do: lower, else: encoder.(lower)
+        upper = if is_atom(upper), do: upper, else: encoder.(upper)
+        Postgrex.Extensions.Range.encode(range, lower, upper)
+      end)
+
+    num_ranges = length(ranges)
+    iodata = [<<num_ranges::int32()>> | encoded_ranges]
+    [<<IO.iodata_length(iodata)::int32()>> | iodata]
+  end
+
+  def decode(<<>>, _decoder, acc), do: Enum.reverse(acc)
+
+  def decode(<<len::int32(), encoded_range::binary-size(len), rest::binary>>, decoder, acc) do
+    <<flags, data::binary>> = encoded_range
+
+    decoded_range =
+      case decoder.(data) do
+        [upper, lower] ->
+          Postgrex.Extensions.Range.decode(flags, [lower, upper])
+
+        empty_or_one ->
+          Postgrex.Extensions.Range.decode(flags, empty_or_one)
+      end
+
+    decode(rest, decoder, [decoded_range | acc])
+  end
+end
