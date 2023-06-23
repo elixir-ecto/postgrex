cdd0a1ec20c00c38421ef75510510eeccb43391c
Nathan Long <him@nathanmlong.com>
2023-06-23 11:06:46 -0400

    Comment on how range retrieval works

--- lib/postgrex/builtins.ex
+++ lib/postgrex/builtins.ex
@@ -52,6 +52,10 @@ defmodule Postgrex.Range do
   @moduledoc """
   Struct for PostgreSQL `range`.
 
+  Note that PostgreSQL itself does not return ranges exactly as stored:
+  `SELECT '(1,5)'::int4range` returns `[2,5)`, which is equivalent in terms of the values included in the range ([PostgreSQL docs](https://www.postgresql.org/docs/current/rangetypes.html#RANGETYPES-IO)).
+  When selecting data, this struct simply reflects what PostgreSQL returns.
+
   ## Fields
 
     * `lower`
