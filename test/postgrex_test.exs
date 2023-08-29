444bc2dc85ea307e8cf9e3fc5a3de791c4c5eb18
Wojtek Mach <wojtekmach@users.noreply.github.com>
2023-08-29 14:38:14 +0200

    Add :ssl to :extra_applications (#658)

--- test/postgrex_test.exs
+++ test/postgrex_test.exs
@@ -2,18 +2,6 @@ defmodule PostgrexTest do
   use ExUnit.Case, async: false
   import ExUnit.CaptureLog
 
-  test "start_link/2 raises when :ssl app is required but not started" do
-    on_exit(fn ->
-      Application.start(:ssl)
-    end)
-
-    Application.stop(:ssl)
-
-    assert_raise RuntimeError, ~r"SSL connection can not be established", fn ->
-      Postgrex.start_link(ssl: true, database: "postgrex_test")
-    end
-  end
-
   test "start_link/2 sets search path" do
     # valid argument
     search_path = ["public", "extension"]
